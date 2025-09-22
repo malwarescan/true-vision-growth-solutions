24cc9115e61b68040336d032fab7be3583cf6ea7
Joel Maldonado <domtheworldllc@gmail.com>
2025-09-22 12:13:34 +0200

    Add privacy policy and terms of service routes
    - Added routes for /privacy-policy and /terms-of-service in AppRouter.tsx
    - Privacy policy and terms of service pages already exist and are linked in footer
    - Users can now access legal pages from footer navigation

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { App } from "./App";
+import { PrivacyPolicy } from "./pages/PrivacyPolicy";
+import { TermsOfService } from "./pages/TermsOfService";
+
 export function AppRouter() {
   return <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
           </Routes>
       </BrowserRouter>;
 }
